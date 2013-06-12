8150afd5d7c7d5e22a42c45611125bde1ae38c2d
Jonathan Lee <joncslee@gmail.com>
2013-06-12 17:59:09 -0400

    enforce color_category validation

--- app/models/diamond.rb
+++ app/models/diamond.rb
@@ -1,9 +1,13 @@
 class Diamond < ActiveRecord::Base
   attr_accessible :clarity, :color, :description, :fluorescence, :gia_report_number, :measurements, :name, :polish, :quantity, :shape, :symmetry, :table_size, :total_depth, :weight, :price, :photo, :certificate, :color_category
 
-  validates :name, :presence => true
+  validates :name, :color_category, :presence => true
 
   mount_uploader :photo, PhotoUploader
   mount_uploader :certificate, CertificateUploader
+
+  def color_category_url
+    "/#{color_category}-diamonds"
+  end
   
 end
