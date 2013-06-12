e3283a2e00666048619e086d2fb21b6a65c0c66e
Jonathan Lee <joncslee@gmail.com>
2013-06-12 16:05:13 -0400

    fixed column name fluorescence

--- app/models/diamond.rb
+++ app/models/diamond.rb
@@ -1,5 +1,5 @@
 class Diamond < ActiveRecord::Base
-  attr_accessible :clarity, :color, :description, :flourescence, :gia_report_number, :measurements, :name, :polish, :quantity, :shape, :symmetry, :table_size, :total_depth, :weight, :price, :photo, :certificate, :color_category
+  attr_accessible :clarity, :color, :description, :fluorescence, :gia_report_number, :measurements, :name, :polish, :quantity, :shape, :symmetry, :table_size, :total_depth, :weight, :price, :photo, :certificate, :color_category
 
   validates :name, :presence => true
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130611015033) do
+ActiveRecord::Schema.define(:version => 20130612200139) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(:version => 20130611015033) do
     t.float    "table_size"
     t.string   "polish"
     t.string   "symmetry"
-    t.string   "flourescence"
+    t.string   "fluorescence"
     t.string   "gia_report_number"
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
