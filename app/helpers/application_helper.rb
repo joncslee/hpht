f8a4038ab10d7ec28eaab59c2f302d8c615d4416
Jonathan Lee <joncslee@gmail.com>
2013-06-11 21:44:17 -0400

    add page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
+  def description(page_description)
+    content = ''
+    content = page_description if page_description.present?
+    content_for(:description) { content }
+  end
+
+  def has_content_for(sym)
+    output = content_for(sym)
+    output.present?
+  end
+
 end
