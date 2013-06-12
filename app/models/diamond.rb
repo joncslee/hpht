e43081b8b9fa2495f31a4fc5b2e5d99af2a0aefc
Jonathan Lee <joncslee@gmail.com>
2013-06-11 21:26:47 -0400

    enforce presence of diamond name only for now

--- app/models/diamond.rb
+++ app/models/diamond.rb
@@ -1,6 +1,8 @@
 class Diamond < ActiveRecord::Base
   attr_accessible :clarity, :color, :description, :flourescence, :gia_report_number, :measurements, :name, :polish, :quantity, :shape, :symmetry, :table_size, :total_depth, :weight, :price, :photo, :certificate, :color_category
 
+  validates :name, :presence => true
+
   mount_uploader :photo, PhotoUploader
   mount_uploader :certificate, CertificateUploader
   
